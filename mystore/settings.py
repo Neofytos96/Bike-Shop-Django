568bf33540018385eb55233f382523cd2a09f65d
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-21 00:09:00 +0100

    README.md
    all

--- mystore/settings.py
+++ mystore/settings.py
@@ -114,8 +114,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-#LOGIN_REDIRECT_URL = '/'
-#CART_SESSION_ID = 'cart'
+LOGIN_REDIRECT_URL = '/'
+CART_SESSION_ID = 'cart'
 
 
 # Internationalization
