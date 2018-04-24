0a3feba5a205ac729f7da6c7c6c27438df95ad99
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-24 21:51:30 +0100

    README.md
    corrections

--- account/models.py
+++ account/models.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 class user(models.Model):
-    username = models.CharField(blank = True, null = True)
-    first_name = models.CharField(blank = True, null = True)
-    last_name = models.CharField(blank = True, null = True)
-    street = models.CharField(blank = True, null = True)
-    city = models.CharField(blank = True, null = True)
-    postcode = models.CharField(blank = True, null = True)
+    username = models.CharField(max_length = 30)
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    street = models.CharField(max_length = 30)
+    city = models.CharField(max_length = 30)
+    postcode = models.CharField(max_length = 30)
     slug = models.SlugField(max_length=30, db_index = True)
 
     def __str__(self):
