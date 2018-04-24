7739907cbaf5e4e8e3e052ec381943ce7ccb531c
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-24 22:42:52 +0100

    README.md
    add details to checkout

--- account/forms.py
+++ account/forms.py
@@ -9,7 +9,7 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
     email = forms.EmailField(max_length=254)
-    street = forms.CharField(max_length=30)
+    address = forms.CharField(max_length=30)
     city = forms.CharField(max_length=30)
     postCode = forms.CharField(max_length=30)
 
