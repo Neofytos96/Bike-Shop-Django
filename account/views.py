f18d35dbedb1e93fda2309b8bb0fa69eeaf66e88
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-25 00:58:14 +0100

    README.md
    try to read new data for user

--- account/views.py
+++ account/views.py
@@ -2,18 +2,23 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 
-from mysite.core.forms import SignUpForm
+from .forms import SignUpForm
 
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            #user.refresh_from_db()
+           # user.profile.address = form.cleaned_data.get('address')
+            #user.profile.city = form.cleaned_data.get('city')
+            #user.profile.postcode = form.cleaned_data.get('postcode')
+            #user.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('/')
     else:
         form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'account/signup.html', {'form': form})
