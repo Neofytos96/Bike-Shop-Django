c355acfb1454cf4d268e1bae14b064f691217ad9
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-24 18:25:18 +0100

    README.md
    add

--- account/models.py
+++ account/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class user(models.Model):
+    name = models.TextField(blank = True, null = True)
+    surname = models.TextField(blank = True, null = True)
+    street = models.TextField(blank = True, null = True)
+    city = models.TextField(blank = True, null = True)
+    postcode = models.TextField(blank = True, null = True)
+
+   
