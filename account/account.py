0fcc26ae4ce3439245042fe6e62c935036700066
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-24 18:01:03 +0100

    README.md
    add

--- account/account.py
+++ account/account.py
@@ -3,9 +3,11 @@ from django.conf import settings
 from books.models import Book
 
 class Account(object):
-    def __init__(self, request):
-        """
-        Initialize the cart.
-        """
-    def get_full_name(self):
-        return render(user.get_full_name)
+ 
+    def get_user_label(self, user):
+        name = user.get_full_name()
+        username = user.username
+        if not self.always_show_username:
+            return name or username
+        return (name and name != username and '%s (%s)' % (name, username)
+                or username)
