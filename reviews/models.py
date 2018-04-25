6893f7473c3d946283979fc8bd52d1c869389ed3
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-25 23:38:30 +0100

    README.md
    reviews

--- reviews/models.py
+++ reviews/models.py
@@ -26,7 +26,7 @@ class Review(models.Model):
         (4, '4'),
         (5, '5'),
     )
-    wine = models.ForeignKey(Wine)
+    bike = models.ForeignKey(Bike)
     pub_date = models.DateTimeField('date published')
     user_name = models.CharField(max_length=100)
     comment = models.CharField(max_length=200)
