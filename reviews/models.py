5022014ed5bddf29fc5af0bb9df39a52605054c8
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-25 23:34:07 +0100

    README.md
    reviews

--- reviews/models.py
+++ reviews/models.py
@@ -16,3 +16,18 @@ class Bike(models.Model):
     pub_date = models.DateTimeField('date published')
     user_name = models.CharField(max_length=100)
     comment = models.CharField(max_length=200)
+
+
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+    wine = models.ForeignKey(Wine)
+    pub_date = models.DateTimeField('date published')
+    user_name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=RATING_CHOICES)
