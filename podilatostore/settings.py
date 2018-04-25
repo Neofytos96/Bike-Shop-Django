4ec934107c8125417cb358bd29ad4ea6b2898169
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-25 23:02:25 +0100

    README.md
    reviews

--- podilatostore/settings.py
+++ podilatostore/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'bikes',
     'cart',
     'order',
+    'reviews', 
 ]
 
 MIDDLEWARE = [
---
+++ reviews/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Bike, Review
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ('bike', 'rating', 'user_name', 'comment', 'pub_date')
+    list_filter = ['pub_date', 'user_name']
+    search_fields = ['comment']
+    
+admin.site.register(Bike)
+admin.site.register(Review, ReviewAdmin)
