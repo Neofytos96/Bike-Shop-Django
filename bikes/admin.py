1ceb9b2cb1ed61b1640b08dba9d7bd8dc7d261b5
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-25 17:17:10 +0100

    README.md
    bikes

--- bikes/admin.py
+++ bikes/admin.py
@@ -16,6 +16,6 @@ class BikeAdmin(admin.ModelAdmin):
 admin.site.register(Bike, BikeAdmin)
 
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ['brand_name', 'second_name', 'slug']
-    prepopulated_fields = {'slug': ('brand_name',)}
+    list_display = ['first_name', 'last_name', 'slug']
+    prepopulated_fields = {'slug': ('last_name',)}
 admin.site.register(Brand, BrandAdmin)
--- bikes/models.py
+++ bikes/models.py
@@ -3,12 +3,12 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 class Brand(models.Model):
-    brand_name = models.CharField(max_length = 20)
-    second_name = models.CharField(max_length = 30)
+    first_name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 30)
     slug = models.SlugField(max_length=30, db_index = True)
 
     def __str__(self):
-        return '%s %s' % (self.brand_name, self.second_name)
+        return '%s %s' % (self.first_name, self.last_name)
         
 class Category(models.Model):
     category = models.CharField(max_length = 50)
