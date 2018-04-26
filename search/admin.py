3951cae340219efb606e60f94772c3c7b21e6828
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-26 02:01:10 +0100

    README.md
    search

--- search/admin.py
+++ search/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from ecomstore.search.models import SearchTerm
+from podilatostore.search.models import SearchTerm
 class SearchTermAdmin(admin.ModelAdmin):
  		list_display = ('__unicode__','ip_address','search_date')
  		list_filter = ('ip_address', 'user', 'q')
