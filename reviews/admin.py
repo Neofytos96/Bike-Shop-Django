e12f9ea08aee904fa381cfe83c8340113f6c0632
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-26 16:28:28 +0100

    README.md
    reviews attemp 1

---
+++ reviews/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from bikes.models import Bike
+from .models import Review
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ('bike', 'user_name', 'comment', 'pub_date')
+    list_filter = ['pub_date', 'user_name']
+    search_fields = ['comment']
+    
+#admin.site.register(Bike)
+admin.site.register(Review, ReviewAdmin)
---
+++ reviews/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from bikes.models import Bike
+
+
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+    bike = models.ForeignKey(Bike)
+    pub_date = models.DateTimeField('date published')
+    user_name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=RATING_CHOICES)
