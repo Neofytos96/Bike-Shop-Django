adab86f70584f0b376ad622b464dba0e8afd0e37
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-27 00:15:08 +0100

    README.md
    review changed to older

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^create/$', views.add_review, name='add_review'),
+    #url(r'^create/$', views.add_review, name='add_review'),
+    url(r'^bike/(?P<bike_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
 ]
