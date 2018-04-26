b1e20698a9e024311bd1d347e19583dc7477eaec
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-26 23:25:14 +0100

    README.md
    review changed to older

--- reviews/views.py
+++ reviews/views.py
@@ -24,10 +24,10 @@ def review_detail(request, review_id):
 
 
 def add_review(request, bike_id):
-    cart = Cart(request)
+    #cart = Cart(request)
     bike = get_object_or_404(Bike, id=bike_id)
-    user_id = request.user.id
-    current_user_object = User.objects.get(id=user_id)
+    #user_id = request.user.id
+    #current_user_object = User.objects.get(id=user_id)
     form = ReviewCreateForm(request.POST)
     if form.is_valid():
         comment = form.cleaned_data['comment']
@@ -41,7 +41,7 @@ def add_review(request, bike_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('reviews:bike_detail', args=(bike.id,)))
+        return HttpResponseRedirect(reverse('reviews:review_detail', args=(bike.id,)))
 
-    return render(request, 'reviews/bike_detail.html', {'bike': bike, 'form': form})
+    return render(request, 'reviews/cerate.html', {'bike': bike, 'form': form})
 
