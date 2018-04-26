7b4dae48c58390eb202cfa2d98f06f594f7342f8
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-26 21:39:44 +0100

    README.md
    reviewing

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from bikes.models import Bike
-from .models import Review
+from .models import Review, ReviewItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = ReviewItem
+    raw_id_fields = ['bike']
 
 class ReviewAdmin(admin.ModelAdmin):
     model = Review
-    list_display = ('bike', 'user_name', 'comment', 'pub_date')
-    list_filter = ['pub_date', 'user_name']
+    list_display = ('bike', 'user_name', 'comment', 'pub_date', 'created')
+    list_filter = ['pub_date', 'user_name ', 'created']
     search_fields = ['comment']
     
 #admin.site.register(Bike)
