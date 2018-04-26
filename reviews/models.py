14b8a21e02d5999402a503079ad880e1b0b0d21b
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-27 00:32:14 +0100

    README.md
    review changed to older

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from bikes.models import Bike
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 
 
 class Review(models.Model):
@@ -10,8 +12,17 @@ class Review(models.Model):
         (4, '4'),
         (5, '5'),
     )
-    bike = models.ForeignKey(Bike)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     pub_date = models.DateTimeField('date published')
     user_name = models.CharField(max_length=100)
     comment = models.CharField(max_length=200)
     rating = models.IntegerField(choices=RATING_CHOICES)
+
+    class Meta:
+        reviewing = ('-created',)
+
+    def __str__(self):
+        return 'Review {}'.format(self.id)
+
+    def get_review(self):
+        return self.items.all()
