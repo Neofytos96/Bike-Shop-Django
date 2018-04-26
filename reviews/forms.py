c84f0339c093bc700d1562bec17c86b3e75ab36c
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-26 01:15:58 +0100

    README.md
    reviews changed a lot

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,17 +1,17 @@
-from django.forms import Textarea
-from django import forms
+from django.forms import ModelForm, Textarea
+#from reviews.models import Review
 from django.core.exceptions import ValidationError
 
 from django.contrib.auth.models import User
 from .models import Review
 
-class ReviewCreateForm(forms.ModelForm):
+class ReviewCreateForm(ModelForm):
 
 	class Meta:
 		model = Review
-		fields = ('review')
+		fields = ['review']
 		widgets = {
             'review': Textarea(attrs={'cols': 40, 'rows': 15})
         }
 
-	
+
