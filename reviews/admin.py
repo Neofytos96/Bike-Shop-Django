9a6f210f4acc802987e913d9bb676f907eac1528
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-26 22:59:59 +0100

    README.md
    reviewing

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,9 +3,9 @@ from bikes.models import Bike
 from .models import Review, ReviewItem
 
 
-class OrderItemInline(admin.TabularInline):
-    model = ReviewItem
-    raw_id_fields = ['bike']
+#class OrderItemInline(admin.TabularInline):
+#    model = ReviewItem
+#    raw_id_fields = ['bike']
 
 class ReviewAdmin(admin.ModelAdmin):
     model = Review
