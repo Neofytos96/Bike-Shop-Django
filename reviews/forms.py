37cc352e744a00a9db60398ca6edaa077c06f3f2
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-27 12:34:36 +0100

    README.md
    review still unfinished

--- reviews/forms.py
+++ reviews/forms.py
@@ -7,15 +7,20 @@ from .models import Review
 class ReviewCreateForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['user_name', 'comment', 'pub_date']
+        fields = ('user_name', 'comment', 'pub_date')
     
 
     def clean_user_name(self):
+    	user_name = self.cleaned_data['user_name']
     	return user_name
 
 
     def clen_comment(self):
+    	comment = self.cleaned_data['comment']
     	return comment
 
     def clean_pub_date():
+
+    	pub_date = self.cleaned_data['pub_date']
+
     	return pub_date
