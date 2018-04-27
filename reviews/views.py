f0bda26104ff44805a594cf296f0db908ff1963d
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-27 20:41:18 +0100

    README.md
    review almost done

--- reviews/views.py
+++ reviews/views.py
@@ -1,16 +1,11 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
-from .models import Review
 from django.shortcuts import render_to_response, get_object_or_404, render
 from django.conf import settings
 from django.http import HttpResponse
-from bikes.models import Bike
-from .forms import ReviewCreateForm
-import datetime
-from cart.cart import Cart
 from django.contrib.auth.models import User
 from .models import Review
+from bikes.models import Bike
+from .forms import ReviewCreateForm
+
 
 
 
@@ -29,34 +24,20 @@ from .models import Review
 
 
 def add_review(request):
-    cart = Cart(request)
+    #cart = Cart(request)
 
     #bike = get_object_or_404(Bike, id=bike_id)
-    user_id = request.user.id
-    current_user_object = User.objects.get(id=user_id)
-    form = ReviewCreateForm(request.POST)
-    if form.is_valid():
-        comment = form.cleaned_data['comment']
-        #user_name = form.cleaned_data['user_name']
-        rating = form.cleaned_data['rating']
-        review.user = current_user_object
-        review = Review()
-        #review.bike = bike
-        #review.user_name = user_name
-        #review.comment = comment
-#        bike = get_object_or_404(Bike, id=bike_id)
- #       review.pub_date = datetime.datetime.now()
-        #review.user = current_user_object
-        review.save()
-        for item in cart:
-            ReviewItem.objects.create(review=review,
-                                      bike=item['bike'],
-                                      rating=item['rating'],
-                                      comment=item['comment'])
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        #return HttpResponseRedirect(reverse('reviews:bike_detail', args=(bike.id,)))
+    if request.user.is_authenticated():
+        user_id = request.user.id
+        current_user_object = User.objects.get(id=user_id)
+        form = ReviewCreateForm(request.POST)
+        if form.is_valid():
+            comment = form.cleaned_data['comment']
+            rating = form.cleaned_data['rating']
+            review.user = current_user_object
+            review = Review()
+            review.save()
+            
         return render (request, 'reviews/reviews/created.html',{'review':review})
     else:
         return render(request, 'reviews/reviews/create_review.html',{'form': form})
