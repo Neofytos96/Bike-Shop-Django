a06df2cc61a045a508011be8fb63a48f36d6aba1
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-27 19:16:35 +0100

    README.md
    review almost done

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from bikes.models import Bike
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+
 
 
 class Review(models.Model):
