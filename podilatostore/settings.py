75dbcda2210d58a11af9ffe44fa82581a438a7c0
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-29 18:38:03 +0100

    README.md
    search first

--- podilatostore/settings.py
+++ podilatostore/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'podilatostore.search',
+    'search',
     'account',
     'bikes',
     'cart',
