f9190a8272029377c4c069f65329abc7a1238af6
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-29 23:02:40 +0100

    README.md
    search

--- bikes/views.py
+++ bikes/views.py
@@ -69,6 +69,33 @@ def search(request):
   pages = pagination(request, results, num=1)
   context = {'items':pages[0], 'page_range':pages[1],}
   return render(request, template, context)
+
+
+import operator
+
+from django.db.models import Q
+
+
+class BlogSearchListView(BlogListView):
+    """
+    Display a Blog List page filtered by the search query.
+    """
+    paginate_by = 10
+
+    def get_queryset(self):
+        result = super(BlogSearchListView, self).get_queryset()
+
+        query = self.request.GET.get('q')
+        if query:
+            query_list = query.split()
+            result = result.filter(
+                reduce(operator.and_,
+                       (Q(title__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                       (Q(content__icontains=q) for q in query_list))
+            )
+
+        return result
   
 
 
