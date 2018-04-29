d97a1ccd7cce8b8f92e3997ed9d0132911b76251
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-29 18:29:04 +0100

    README.md
    search first

--- podilatostore/search/filters.py
+++ podilatostore/search/filters.py
@@ -1,7 +1,14 @@
-from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.models import User, Bike
+
 import django_filters
 
+
 class UserFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(lookup_expr='icontains')
+    bike = django_filters.CharFilter(lookup_expr='icontains')
+    brand = django_filters.ModelMultipleChoiceFilter(queryset=Brands.objects.all(), widget=forms.CheckboxSelectMultiple)
+
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', ]
+        fields = ['username', 'first_name', 'bike', 'brand']
