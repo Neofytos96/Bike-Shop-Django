43adbdbb90730169d35e0f980ae1a99a533e60e4
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-29 19:15:25 +0100

    README.md
    sort

--- bikes/views.py
+++ bikes/views.py
@@ -24,3 +24,28 @@ def bike_detail(request, id, slug):
                    {'bike': bike,
                     'cart_bike_form': cart_bike_form})
                   #{'book': book})
+
+def sort(request):
+  brand=None
+  select_type = request.POST.get("select_type")
+  select_criteria = request.POST.get("select_criteria")
+
+  bikes = Bike.objects.all()
+  brands = Brand.objects.all()
+  if select_type =="all":
+    return render(request, 'bikes/bike/list.html', {'brand': brand,'brands': brands,'bikes': bikes})
+  elif select_type =="alphabetically":
+    if select_criteria=="High to Low":
+      bikes= Bike.objects.order_by("title")
+      return render(request, 'bikes/bike/list.html', {'brand': brand,'brands': brands,'bikes': bikes})
+    else:
+      bikes= Bike.objects.order_by("-title")
+      return render(request, 'bikes/bike/list.html', {'brand': brand,'brands': brands,'bikes': bikes})
+
+  elif select_type == "price" :
+    if select_criteria=="Low to High":
+      bikes= Bike.objects.order_by("price")
+      return render(request, 'bikes/bike/list.html', {'brand': brand,'brands': brands,'bikes': bikes})
+    else:
+      bikes= Bike.objects.order_by("-price")
+      return render(request, 'bikes/bike/list.html', {'brand': brand,'brands': brands,'bikes': bikes})
