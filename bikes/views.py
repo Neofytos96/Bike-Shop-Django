5360912238ffc82088719aaa6a4839a46d6a8833
Neofytos96 <32684349+Neofytos96@users.noreply.github.com>
2018-04-30 02:44:05 +0100

    README.md
    please search work

--- bikes/views.py
+++ bikes/views.py
@@ -113,7 +113,7 @@ def search(request):
     page = request.GET.get(page_request_var)
     try: 
       queryset = paginator.page(page)
-    except:
+    except PageNotAnInteger:
       queryset = paginator.page(1)
     except EmptyPage:
       queryset = paginator.page(paginator.num_pages)
